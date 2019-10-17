4716302b0f1ad346f8becc2a170eff14eba44e30
jakub.krhovjak@pricefx.eu <jakub@krhovjak.pricefx.eu>
2019-10-17 06:04:46 +0200

    m2

--- reactive-producer/src/main/java/com/example/reactiveproducer/security/LogFilter.java
+++ reactive-producer/src/main/java/com/example/reactiveproducer/security/LogFilter.java
@@ -18,6 +18,8 @@ import lombok.extern.slf4j.Slf4j;
 public class LogFilter implements Filter {
 
 
+
+
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         log.info("Log filter");
