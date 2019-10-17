864c3d36ffce4ecee9a667806b64ea8555301722
jakub.krhovjak@pricefx.eu <jakub@krhovjak.pricefx.eu>
2019-10-17 05:48:19 +0200

    merge1

--- reactive-producer/src/main/java/com/example/reactiveproducer/controller/SecController.java
+++ reactive-producer/src/main/java/com/example/reactiveproducer/controller/SecController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
+
 public class SecController {
 
 
@@ -21,11 +22,4 @@ public class SecController {
     public String free() {
         return "ok";
     }
-
-
-
-
-
-
 }
-//commnet
