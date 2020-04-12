2d9237d9dbf2573140ccf32a9c2a1dd32b594b8e
Jakub Krhovjak <jakub.krhovjak@pricefx.eu>
2020-04-12 07:03:10 +0200

    aded validation

--- client/src/component/Login/Login.tsx
+++ client/src/component/Login/Login.tsx
@@ -1,46 +1,75 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Paper from "@material-ui/core/Paper";
 import Box from "@material-ui/core/Box";
 import Button from "@material-ui/core/Button";
 import { Formik, Form, Field } from "formik";
 import { TextField } from "formik-material-ui";
+import * as Yup from "yup";
 
 import { restService } from "../../client/restClient";
 import { useRouter } from "react-router5";
 
+const loginSchema = Yup.object().shape({
+    username: Yup.string().email("Invalid email").required("Required"),
+    password: Yup.string().required("password", "Required"),
+});
+
 export const Login = (props) => {
     const router = useRouter();
 
+    const add = (a, b) => {
+        return a + b;
+    };
+
+    useEffect(() => {
+        console.log(add(2, 3));
+    }, []);
+
     const authenticate = (values) => {
-        restService.authenticate(values.username, values.password)
-            .then(res => {
+        restService
+            .authenticate(values.username, values.password)
+            .then((res) => {
                 router.navigate("basic");
             })
-            .catch(e => {
+            .catch((e) => {
                 throw new Error("eeeeee");
             });
     };
 
     return (
-        <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center"
-             style={{ width: "100%", height: "100%" }}>
+        <Box
+            display="flex"
+            flexDirection="column"
+            justifyContent="center"
+            alignItems="center"
+            style={{ width: "100%", height: "100%" }}
+        >
             <Paper elevation={3}>
-                <Formik initialValues={{ username: "", password: "" }} onSubmit={(values, props) => {
-                    props.setSubmitting(false);
-                    authenticate(values);
-                }}>
+                <Formik
+                    validationSchema={loginSchema}
+                    initialValues={{ username: "", password: "" }}
+                    onSubmit={(values, props) => {
+                        props.setSubmitting(false);
+                        authenticate(values);
+                    }}
+                >
                     {(props) => (
                         <Form>
-                            <Box display="flex" flexDirection="column" justifyContent="center" m={1} mt={0}
-                                 style={{ height: "200px" }}>
+                            <Box
+                                display="flex"
+                                flexDirection="column"
+                                justifyContent="center"
+                                m={1}
+                                mt={0}
+                                style={{ height: "200px" }}
+                            >
                                 <Box p={2} pt={0}>
                                     <Field
                                         component={TextField}
                                         name="username"
                                         type="email"
                                         label="Username"
-                                    >
-                                    </Field>
+                                    ></Field>
                                 </Box>
 
                                 <Box p={2}>
@@ -49,8 +78,7 @@ export const Login = (props) => {
                                         name="password"
                                         type="password"
                                         label="Password"
-                                    >
-                                    </Field>
+                                    ></Field>
                                 </Box>
                                 <Button
                                     variant="contained"
@@ -61,13 +89,10 @@ export const Login = (props) => {
                                     Login
                                 </Button>
                             </Box>
-
-                        </Form>)}
-
+                        </Form>
+                    )}
                 </Formik>
             </Paper>
         </Box>
     );
-
 };
-
